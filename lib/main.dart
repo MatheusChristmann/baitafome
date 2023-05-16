d0f8ef261cf898792c0e4cad07cd2af902143ea8
Matheus Felipe Christmann <matheus.ch35@hotmail.com>
2023-05-16 20:19:52 -0300

    #Utilizando o WindowsManager

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:baitafome/pages/mainpage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:window_manager/window_manager.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
   runApp(const BaitaFomeApp());
 }
 
