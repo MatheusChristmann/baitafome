a91a412613b1ff61981c2c72e0f1103274d599a3
lucasbaronspier <lucasbaronspier@gmail.com>
2023-06-21 20:21:27 -0300

    Adicionado teste para o Card e ChildScrollView nas Categorys

--- test/BaitaFome_test.dart
+++ test/BaitaFome_test.dart
@@ -1,15 +1,15 @@
 import 'package:baitafome/models/recipe.dart';
 import 'package:baitafome/models/type.dart';
+import 'package:baitafome/pages/categorys.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
-  group('BaitaFome Test', () {     
-    test('Create Type and Recipe Test', (){
-      Type type = Type(
-        id: 7,
-        description: 'Description Type Test'
-      );      
+  group('BaitaFome Test', () {
+    test('Create Type and Recipe Test', () {
+      Type type = Type(id: 7, description: 'Description Type Test');
 
       Recipe recipe = Recipe(
         id: 10,
@@ -24,11 +24,20 @@ void main() {
       expect(recipe.type, 7);
     });
 
+    testWidgets('Categorys have at least one Card', (tester) async {
+      await tester.pumpWidget(MaterialApp(home: Scaffold(body: Categorys())));
+      //await tester.pumpWidget(const SplashPage());
+      final hasIndicator = find.byType(Card);
 
-  /*testWidgets('Find Text Test', (tester) async {
-    await tester.pumpWidget();
-    expect(find.byType(FloatingActionButton), findsOneWidget);
-  });*/
+      expect(hasIndicator, findsWidgets);
+    });
 
+    testWidgets('Categorys dont have SingleChildScrollView', (tester) async {
+      await tester.pumpWidget(MaterialApp(home: Scaffold(body: Categorys())));
+      //await tester.pumpWidget(const SplashPage());
+      final hasIndicator = find.byType(SingleChildScrollView);
+
+      expect(hasIndicator, findsNothing);
+    });
   });
 }
